05e3d241888bdb124dbb69f89f98b94150041a9c
learn <learn@LEARN-09.local>
2020-02-20 16:49:39 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component{
   render(){
     return(
       <div>
-      <h1> Treasure Map! </h1>
+      <h1> Welcome to our treasure map game!</h1>
+        <p>FIND THE TREASURE OR FIND YOUR DEATH!</p>
         <Board />
-
       </div>
     )
   }
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,21 +5,54 @@ class Board extends Component {
     constructor (props) {
         super (props)
         this.state = {
-            board: ["🌱","🌱","🌱","💰","🌱","🌱","🌱","🌱","🌱",]
+            shown: ["?","?","?","?","?","?","?","?","?",],
+            board: this.randomizeArray(),
+            treasure: null,
+            bomb: null,
+            tree: null,
+            counter: 5,
+            isWinner: ""
+
         }
     }
 
-    handleLocation = (index) => {
-        alert (index)
+    countRemaining = () => {
+        let counter = this.state.counter
+        this.setState({counter: counter -1})
+        if(this.state.counter <= 1) {
+            alert("YOU DONE F'D UP")
+            window.location.reload()
+        }
     }
 
-    randomizer = () => {
-            let randomTreasure = Math.floor(Math.random() * this.state.board.length)
-            this.setState({ board: randomTreasure })
+    onClickReload = () => {
+        window.location.reload()
+    }
+
+    handleLocation = (index) => {
+        let {shown, board} = this.state
+        shown[index] = board[index]
+        this.setState({
+            shown: shown,
+            board: board,
+        })
+    }
+
+
+    randomizeArray() {
+        let tempArray = Array(9).fill("🌴")
+        let treasure = Math.floor(Math.random() * tempArray.length)
+        let bomb = Math.floor(Math.random() * tempArray.length)
+        while (treasure === bomb) {
+             bomb = Math.floor(Math.random() * tempArray.length)
+        }
+        tempArray[treasure] = "💰"
+        tempArray[bomb] = "💣"
+        return tempArray;
     }
 
     render () {
-        let square = this.state.board.map((value, index) => {
+        let square = this.state.shown.map((value, index) => {
             return (
                 <Square
                     handleLocation = {this.handleLocation}
@@ -30,10 +63,13 @@ class Board extends Component {
         })
 
         return (
-            <div id= "board">
+        <div>
+            <div id = "guess"> Remaining guesses: {this.state.counter}</div>
+            <div id= "board" onClick={this.countRemaining}>
             { square }
-
+            <button onClick= {this.onClickReload}>RESET YE GAME</button>
             </div>
+        </div>
         )
     }
 }
